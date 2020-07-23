0417b0d2e63c2b729619acb7ae36835aa9b03a2c
Rohan-shakya <rohanshakya254@gmail.com>
2020-07-23 19:08:46 +0545

    added 404 page

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
+import { NavLink } from 'react-router-dom';
 
 export default class Default extends Component {
   render() {
-    return <h4>Helo from Error page</h4>;
+    return (
+      <Header title='404' styleClass='default-hero'>
+        <h2 className='text-uppercase text-light'>
+          You are in the wrong place
+        </h2>
+        <NavLink
+          to='/'
+          className='text-uppercase btn btn-secondary btn-lg mt-3'
+        >
+          Return Home
+        </NavLink>
+      </Header>
+    );
   }
 }
