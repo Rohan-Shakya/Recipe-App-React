e5352b7fdc3dcd6f255f4582a589e74e4c3f6316
Rohan-shakya <rohanshakya254@gmail.com>
2020-07-23 14:54:04 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  // const APP_ID = '4b6d17d8';
+  // const APP_KEY = 'd6d64d5899a23dfcaeea500b1b4ae695';
+  // const response = await fetch(
+  //   `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+  // );
+  // const data = await response.json();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hellp</h1>
     </div>
   );
 }
