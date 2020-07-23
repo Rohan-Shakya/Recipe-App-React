435b11098223600f9ebc4c4ff63c509800059067
Rohan-shakya <rohanshakya254@gmail.com>
2020-07-23 18:54:38 +0545

    added home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
+import { NavLink } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
-    return <h4>Hello from Home Page</h4>;
+    return (
+      <Header title='amazing recipes'>
+        <NavLink
+          exact
+          activeClassName='nav-active'
+          to='/recipes'
+          className='text-uppercase btn btn-secondary btn-lg mt-3'
+        >
+          Search Recipes
+        </NavLink>
+      </Header>
+    );
   }
 }
