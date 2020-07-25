89f7edb3e34f8116b1506abdb328906182b7e104
Rohan-shakya <rohanshakya254@gmail.com>
2020-07-25 09:24:23 +0545

    Updates

---
+++ precache-manifest.6582382a7c780426e9cd973341c8b37a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c07259ef830649c4ecec56a2a41e55",
+    "url": "/Recipe-App-React/index.html"
+  },
+  {
+    "revision": "576d7560a9cb9e59aed9",
+    "url": "/Recipe-App-React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c073b181d1ff63ab4f78",
+    "url": "/Recipe-App-React/static/css/main.5079f388.chunk.css"
+  },
+  {
+    "revision": "576d7560a9cb9e59aed9",
+    "url": "/Recipe-App-React/static/js/2.7de5b809.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Recipe-App-React/static/js/2.7de5b809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c073b181d1ff63ab4f78",
+    "url": "/Recipe-App-React/static/js/main.4423bb80.chunk.js"
+  },
+  {
+    "revision": "75640ff222d4a6296b17",
+    "url": "/Recipe-App-React/static/js/runtime-main.572af77a.js"
+  },
+  {
+    "revision": "ef13e20031b028a3aabc20f19d74e008",
+    "url": "/Recipe-App-React/static/media/close-up-of-salad-on-table-326281.ef13e200.jpg"
+  },
+  {
+    "revision": "c9459194545e1916abda50792755cb8d",
+    "url": "/Recipe-App-React/static/media/plates-with-bagel-with-asparagus-and-boiled-egg-and-toasts-4013765.c9459194.jpg"
+  }
+]);
